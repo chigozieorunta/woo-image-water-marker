74eb8fb9478b827e443fc74e28b119a086c72e96
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-06-29 13:16:12 +0100

    Changed method name, parameters and ID variable to class variable

--- src/Watermark.php
+++ src/Watermark.php
@@ -112,20 +112,4 @@ class Watermark {
 			plugin_dir_url( __DIR__ ) . 'images/woo-image-water-marker-' . $this->id . '.jpg'
 		);
 	}
-
-	/**
-	 * Get image's absolute path
-	 *
-	 * @param integer $id
-	 * @return string
-	 */
-	public function get_image_absolute_path( int $id ): string {
-		// Get absolute path
-		$woo_image_url   = wp_get_attachment_url( $id );
-		$img_uploads_dir = wp_upload_dir();
-
-		$this->path = str_replace( $img_uploads_dir['baseurl'], $img_uploads_dir['basedir'], $woo_image_url );
-
-		return $this->path;
-	}
 }
