4e4cb0e5105fb96375224de0a889b8fa7f6465e5
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-29 16:49:34 +0100

    Plugin class for establishing new instance and activating watermark

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,6 +5,8 @@
 
 namespace WooImageWaterMarker;
 
+use Imagine\Imagick\Imagine;
+
 /**
  * Plugin Class
  */
@@ -15,7 +17,14 @@ class Plugin {
 	private static $instance;
 
 	/**
-	 * Undocumented function
+	 * Imagine Object
+	 *
+	 * @var object
+	 */
+	private object $imagine;
+
+	/**
+	 * Instantiate Plugin
 	 *
 	 * @return object
 	 */
@@ -32,16 +41,51 @@ class Plugin {
 	 *
 	 * @return void
 	 */
-	public function activate(): void {
-		add_action( 'init', [ $this, 'register_your_distress' ] );
+	public function activate() : void {
+		add_filter( 'init', [ $this, 'get_imagine' ] );
+		add_filter( 'woocommerce_product_get_image', [ $this, 'get_image_watermark_html' ], 10, 2 );
+		//add_filter( 'woocommerce_single_product_image_thumbnail_html', [ $this, 'get_image_watermark_html' ] );
 	}
 
 	/**
-	 * Register your distress
+	 * Register Imagine Object
 	 *
 	 * @return void
 	 */
-	public function register_your_distress(): void {
-		echo 'Hello World';
+	public function get_imagine() : void {
+		$imagine = new Imagine();
+		$imagine->open( __DIR__ . '/images/gozie.jpg' );
+		$imagine->save( __DIR__ . '/images/woo-image-water-marker.jpg' );
+		$this->imagine = $imagine;
+	}
+
+	/**
+	 * Return thumbnail watermark
+	 *
+	 * @return string
+	 */
+	public function get_image_watermark_html( string $img_html, object $image ) : string {
+		// Get image ID
+		$woo_image = wp_get_attachment_image_url( $image->get_image_id(), 'woocommerce_thumbnail' );
+
+		// Get Imagine object
+		//$imagine = new Imagine();
+		//$imagine = new \Imagine\Imagick\Imagine();
+		//$imagine->open( __DIR__ . '/images/gozie.jpg' );
+
+		//$watermark = $imagine->open( __DIR__ . '/images/watermark.jpeg' );
+		//$image     = $imagine->open( $woo_image );
+		//$size      = $image->getSize();
+		//$wSize     = $watermark->getSize();
+
+		//$bottomRight = new Imagine\Image\Point( $size->getWidth() - $wSize->getWidth(), $size->getHeight() - $wSize->getHeight() );
+		//$image->paste( $watermark );
+		//$image->save( __DIR__ . '/images/woo-image-water-marker.jpg' );
+
+		/*return sprintf(
+			'<img src="%1$s" />',
+			plugin_dir_url( __DIR__ ) . 'images/woo-image-water-marker.jpg'
+		);*/
+		return '';
 	}
 }
