b4f5ab18d52192dbb7533ee1cf74b3511dab66e8
Chigozie Orunta <chigozieorunta@yahoo.com>
2023-03-03 18:57:38 +0100

    Added new plugin entry point

---
+++ src/Plugin.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin entry point.
+ *
+ * @package WooImageWaterMarker
+ */
+
+namespace WooImageWaterMarker;
+
+use WooImageWaterMarker\Watermark;
+
+/**
+ * Plugin Class
+ */
+class Plugin {
+	/**
+	 * Plugin instance.
+	 *
+	 * @var Plugin
+	 */
+	private static $instance;
+
+	/**
+	 * Instantiate Plugin.
+	 *
+	 * @return \Plugin
+	 */
+	public static function init() {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Run Watermark plugin.
+	 *
+	 * @return void
+	 */
+	public function run(): void {
+		$watermark = Watermark::get_instance();
+		$watermark->run();
+	}
+}
