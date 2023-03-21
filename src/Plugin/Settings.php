1f87ba00b641064d7e667067e07d285ecafc8395
Chigozie Orunta <chigozieorunta@yahoo.com>
2023-03-21 18:17:36 +0100

    Added settings class

---
+++ src/Plugin/Settings.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Settings.
+ *
+ * @package WooImageWaterMarker
+ */
+
+namespace WooImageWaterMarker\Plugin;
+
+/**
+ * Settings Class.
+ */
+class Settings {
+	/**
+	 * Plugin name.
+	 *
+	 * @const string
+	 */
+	public const NAME = 'Woo Image Water Marker';
+
+	/**
+	 * Plugin slug.
+	 *
+	 * @const string
+	 */
+	public const SLUG = 'woo_image_water_marker';
+
+	/**
+	 * Plugin role.
+	 *
+	 * @const string
+	 */
+	public const ROLE = 'manage_options';
+
+	/**
+	 * Plugin domain.
+	 *
+	 * @const string
+	 */
+	public const DOMAIN = 'woo-image-water-marker';
+}
